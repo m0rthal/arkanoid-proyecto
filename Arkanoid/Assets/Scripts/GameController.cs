13cfaa379e8f17c8cc0f582b9d3a478583c24033
m0rthal <jesusolmedo77@gmail.com>
2017-03-31 00:46:06 -0400

    Agregando proyecto v0.5

---
+++ Arkanoid/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+        public GUIText scoreText;
+        public int score;
+        void Start()
+        {
+                score = 0;
+                updateScore();
+        }
+
+        public void addScore (int newScore)
+        {
+                score += newScore;
+                updateScore();
+        }
+
+	void updateScore ()
+        {
+                scoreText.text = "Score: " + score;
+        }
+}
---
+++ Arkanoid/Assets/Scripts/bloque.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bloque : MonoBehaviour
+{
+        public int score;
+        private GameController gameController;
+
+        void Start()
+        {
+                GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+                if (gameControllerObject != null)
+                {
+                        gameController = gameControllerObject.GetComponent<GameController>();
+                }
+                if (gameController == null)
+                {
+                        Debug.Log("no se encontro script");
+                }
+        } 
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+                gameController.addScore(score);
+                Destroy(gameObject);
+        }
+}
