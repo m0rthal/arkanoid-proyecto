41b071547a7093539898264c7828de808a9a5874
m0rthal <jesusolmedo77@gmail.com>
2017-03-31 04:16:43 -0400

    mejorando

--- Arkanoid/Assets/Scripts/Bola.cs
+++ Arkanoid/Assets/Scripts/Bola.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class Bola : MonoBehaviour {
         public float speed = 100.0f;
+        Vector2 a = new Vector2(0,12.4F);
+        public GameObject barra;
+        Touch toque;
+        private bool jugando = false;
+        private GameController gameController;
         // Use this for initialization
-        void Start () {
-                GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+        void Start ()
+        {
+                GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+                if (gameControllerObject != null)
+                {
+                        gameController = gameControllerObject.GetComponent<GameController>();
+                }
+
+
+                GetComponent<Rigidbody2D>().position = barra.GetComponent<Rigidbody2D>().position + a;
         }
+
+        void Update()
+        {
+                if (!jugando)
+                {
+                        GetComponent<Rigidbody2D>().position = barra.GetComponent<Rigidbody2D>().position + a;
+                        for (var i = 0; i < Input.touchCount; ++i)
+                        {
+                                toque = Input.GetTouch(i);
+                                if (toque.phase == TouchPhase.Began)
+                                {
+                                        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+                                        jugando = true;
+                                }
+                        }
+                } else
+                {
+
+                }
+        }
+
         float hitFactor(Vector2 ballPos, Vector2 racketPos,
                 float racketWidth)
         {
@@ -33,5 +67,16 @@ public class Bola : MonoBehaviour {
                         // Set Velocity with dir * speed
                         GetComponent<Rigidbody2D>().velocity = dir * speed;
                 }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+                Vector2 inicio;
+                inicio.x = 0;
+                inicio.y = -84;
+                gameController.muerte();
+                GetComponent<Rigidbody2D>().position = inicio;
+                jugando = false;
+                Start();
         }
 }
--- Arkanoid/Assets/Scripts/GameController.cs
+++ Arkanoid/Assets/Scripts/GameController.cs
@@ -1,25 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameController : MonoBehaviour {
 
         public GUIText scoreText;
+        public GUIText vidasText;
         public int score;
+        public int vidas = 3;
+        bool nivel2 = false;
         void Start()
         {
-                score = 0;
                 updateScore();
+                updateVidas();
+        }
+
+        void Update()
+        {
+
+                if (score == 1950 && !nivel2)
+                {
+                        Resources.UnloadUnusedAssets();
+                        SceneManager.LoadScene("escena2");
+                }
+
+                if(vidas == 0)
+                {
+                        SceneManager.LoadScene("escena");
+                }
         }
 
         public void addScore (int newScore)
         {
                 score += newScore;
-                updateScore();
+                updateScore();  
+        }
+
+        public void muerte()
+        {
+                vidas--;
+                updateVidas();
         }
 
 	void updateScore ()
         {
                 scoreText.text = "Score: " + score;
+        }
+
+        void updateVidas()
+        {
+                vidasText.text = "Vidas: " + vidas;
         }
 }
